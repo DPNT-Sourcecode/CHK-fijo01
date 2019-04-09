36494399eae192c3b8fb7cfc42c423ef6ff81d08
sbx_user1051 <sbx_user1051@169.254.21.101>
2019-04-09 10:25:04 +0000

    Tue Apr 09 10:25:04 UTC 2019

--- test/HLO/hello_test.rb
+++ test/HLO/hello_test.rb
@@ -7,7 +7,8 @@ Logging.logger.root.appenders = Logging.appenders.stdout
 require_solution 'HLO'
 
 class ClientTest < Minitest::Test
-  def test_hello
+  def test_HLO_R1
     assert_equal 'Hello, World!', Hello.new.hello('World'), 'App says hello to the world'
   end
 end
+
