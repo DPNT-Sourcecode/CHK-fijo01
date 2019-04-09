7c18865932a6fdb4e728b85a97980812f1f337b9
sbx_user1051 <sbx_user1051@169.254.21.101>
2019-04-09 14:47:36 +0000

    Tue Apr 09 14:47:36 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -8,12 +8,12 @@ class Checkout
       'D' => 15,
       'E' => 40
     }
-
+    
     @prices.default = 0
-
+    
     @item_list = []
   end
-
+  
   # +------+-------+------------------------+
   # | Item | Price | Special offers         |
   # +------+-------+------------------------+
@@ -23,41 +23,39 @@ class Checkout
   # | D    | 15    |                        |
   # | E    | 40    | 2E get one B free      |
   # +------+-------+------------------------+
-
+  
   def checkout(skus)
     return -1 unless /^[A-Z]*$/ === skus
-
+    
     checkout_value = 0
     @item_list = skus.chars
     @item_list.each { |item| checkout_value += @prices[item] }
     checkout_value -= discounts(@item_list)
-
+    
     checkout_value
   end
-
+  
   def discounts(item_list)
     discount = 0
-
+    
     quantity_of_A = @item_list.count('A')
-
+    
     if quantity_of_A >= 5
       discount_5A = quantity_of_A / 5
       discount += (discount_5A * 50)
       quantity_of_A -= discount_5A * 5
     end
-
+    
     if quantity_of_A >= 3
       discount_3A = quantity_of_A / 3
       discount += (discount_3A * 20)
     end
-
-    if @item_list.count('E')
-
+    
     discount += (@item_list.count('B') / 2) * 15
-
     discount
   end
 end
+
 
 
 
