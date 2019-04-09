72a4d6502e51f3825b653f9d1387ebd3e53b767f
sbx_user1051 <sbx_user1051@169.254.21.101>
2019-04-09 13:53:36 +0000

    Tue Apr 09 13:53:36 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,6 +1,6 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
-
+  
   # +------+-------+------------------------+
   # | Item | Price | Special offers         |
   # +------+-------+------------------------+
@@ -10,46 +10,53 @@ class Checkout
   # | D    | 15    |                        |
   # | E    | 40    | 2E get one B free      |
   # +------+-------+------------------------+
-
+  
   def checkout(skus)
-
+    
     return -1 unless /^[A-Z]*$/ === skus
-
+    
     checkout_value = 0
-
+    
     prices = {
       'A' => 50,
       'B' => 30,
       'C' => 20,
       'D' => 15
     }
-
+    
     prices.default = 0
-
+    
     item_list = skus.chars
-
+    
     checkout_value -= discounts(item_list)
-
+    
     item_list.each do |item|
       checkout_value += prices[item]
     end
-
+    
     checkout_value
   end
-
+  
   def discounts(item_list)
     discount = 0
-
+    
     quantity_of_A = item_list.count('A')
 
-    until (quantity_of_A / 3) == 0 do
-      c
+    until (quantity_of_A / 5).zero?
+      discounted = item_list.count('A') / 5
+      discount += discounted * 20
+      quantity_of_A -= 5
     end
+    
+    until (quantity_of_A / 3).zero?
 
+    end
+    
     # discount += (item_list.count('A') / 3) * 20
     discount += (item_list.count('B') / 2) * 15
-
+    
     discount
   end
-
+  
 end
+
