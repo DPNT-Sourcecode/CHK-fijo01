af5f80c264fc9543806b873c19b16f335eadc3f3
sbx_user1051 <sbx_user1051@169.254.21.101>
2019-04-09 10:41:05 +0000

    Tue Apr 09 10:41:05 UTC 2019

--- spec/CHK/checkout_spec.rb
+++ spec/CHK/checkout_spec.rb
@@ -1,7 +1,18 @@
 require_solution 'CHK'
 
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
+
 describe Checkout do
-  it "Read the beginning of a story" do
-    expect(Story.new.read).to eq "Once upon a time"
+  it "returns total checkout value of the items" do
+    expect(Checkout.new.checkout('ABCD')).to eq (50+30+20+15)
   end
 end
+
+
