b374cf92b68903806b066a6a0553685deaf44759
sbx_user1051 <sbx_user1051@169.254.21.101>
2019-04-09 10:53:06 +0000

    Tue Apr 09 10:53:06 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,12 +1,23 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
+
   def checkout(skus)
+
     tokenised_sku = skus.chars
 
-    
+
   end
 
 end
+
 
 
