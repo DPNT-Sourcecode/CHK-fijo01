9f0f91e8815395142def986a207b9762656de9c5
sbx_user1051 <sbx_user1051@169.254.21.101>
2019-04-09 14:11:36 +0000

    Tue Apr 09 14:11:36 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -37,16 +37,9 @@ class Checkout
     discount = 0
 
     quantity_of_A = item_list.count('A')
-    puts 'quantity_of_A' + quantity_of_A
-
     discount_5A = quantity_of_A / 5
-    puts discount_5A
-
     quantity_of_A -= discount_5A
-    puts quantity_of_A
-
     discount_3A = quantity_of_A / 3
-    puts discount_3A
 
     discount += (discount_5A * 50)
     discount += (discount_3A * 20)
@@ -58,6 +51,7 @@ class Checkout
   end
   
 end
+
 
 
 
